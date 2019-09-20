18f6d4b1065031ba287dd307c2410816403350f1
12mohaned <mohaned_boss@outlook.com>
2019-09-20 22:03:38 +0200

    Create Database With Table Employees

---
+++ Assignment1/SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE Employee( ID int unique, 
+firstname varchar(250), 
+middlename varchar(250), 
+country varchar(250) ,
+faxnumber int, 
+salary decimal , 
+birthdate DATE, 
+age int );
---
+++ Assignment1/dbo.Employee.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [id]         INT          NULL,
+    [FirstName]  VARCHAR (250)  NULL,
+    [MiddleName] VARCHAR (250)  NULL,
+    [Lastname]   VARCHAR (250)  NULL,
+    [country]    VARCHAR (250)  NULL,
+    [faxNumber]  INT          NULL,
+    [salary]     DECIMAL (18) NULL,
+    [birth_date] DATE         NULL,
+    [age]        INT          NULL,
+    UNIQUE NONCLUSTERED ([id] ASC)
+);
+
